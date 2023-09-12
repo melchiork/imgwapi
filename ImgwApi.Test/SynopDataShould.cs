497d897c6f7b08cb8bccde503e6f3750b4a87ffc
melchiork <2656932+melchiork@users.noreply.github.com>
2023-09-12 22:36:17 +0200

    SynopData now readonly with a ctor

---
+++ ImgwApi.Test/SynopDataShould.cs
@@ -0,0 +1,19 @@
+﻿namespace ImgwApi.Test;
+
+public class SynopDataShould
+{
+    [Theory]
+    [InlineData("2023-09-12", 1, "2023-09-12T01:00:00")]
+    [InlineData("2023-09-12", 10, "2023-09-12T10:00:00")]
+    [InlineData("2023-09-12", 23, "2023-09-12T23:00:00")]
+    public void Ctor_CalculateDateTimeOfMeasurement(string dateText, int hour, string expectedDateText)
+    {
+        var date = DateTime.Parse(dateText);
+        var synopData = new SynopData(1, "a", date, hour, 2, 10, 2, 3, 4, null);
+
+        var expectedDate = DateTime.Parse(expectedDateText);
+
+        synopData.DateTimeOfMeasurement.Should().Be(expectedDate);
+    }
+
+}
--- ImgwApi/SynopData.cs
+++ ImgwApi/SynopData.cs
@@ -5,34 +5,52 @@ namespace ImgwApi
 {
     public class SynopData
     {
+        [JsonConstructor]
+        public SynopData(int stationId, string stationName, DateTime dateOfMeasurement, int hourOfMeasurement, decimal temparatureC, decimal windSpeed, decimal windDirection, decimal relativeHumidity, decimal rainfall, decimal? pressure)
+        {
+            StationId = stationId;
+            StationName = stationName;
+            DateOfMeasurement = dateOfMeasurement;
+            HourOfMeasurement = hourOfMeasurement;
+            TemparatureC = temparatureC;
+            WindSpeed = windSpeed;
+            WindDirection = windDirection;
+            RelativeHumidity = relativeHumidity;
+            Rainfall = rainfall;
+            Pressure = pressure;
+            DateTimeOfMeasurement = dateOfMeasurement.AddHours(hourOfMeasurement);
+        }
+
         [JsonProperty("id_stacji")]
-        public int StationId { get; set; }
+        public int StationId { get; }
 
         [JsonProperty("stacja")]
-        public string StationName { get; set; }
+        public string StationName { get; }
 
         [JsonProperty("data_pomiaru")]
-        public DateTime DateOfMeasurement { get; set; }
+        public DateTime DateOfMeasurement { get; }
 
         [JsonProperty("godzina_pomiaru")]
-        public int HourOfMeasurement { get; set; }
+        public int HourOfMeasurement { get; }
+
+        public DateTime DateTimeOfMeasurement { get; }
 
         [JsonProperty("temperatura")]
-        public decimal TemparatureC { get; set; }
+        public decimal TemparatureC { get; }
 
         [JsonProperty("predkosc_wiatru")]
-        public decimal WindSpeed { get; set; }
+        public decimal WindSpeed { get; }
 
         [JsonProperty("kierunek_wiatru")]
-        public decimal WindDirection { get; set; }
+        public decimal WindDirection { get; }
 
         [JsonProperty("wilgotnosc_wzgledna")]
-        public decimal RelativeHumidity { get; set; }
+        public decimal RelativeHumidity { get; }
 
         [JsonProperty("suma_opadu")]
-        public decimal Rainfall { get; set; }
+        public decimal Rainfall { get; }
 
         [JsonProperty("cisnienie")]
-        public decimal? Pressure { get; set; }
+        public decimal? Pressure { get; }
     }
 }
