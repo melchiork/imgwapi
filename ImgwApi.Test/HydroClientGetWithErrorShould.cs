ff65088ed12d304d4f62acab457a9ab82eb9d394
melchiork <2656932+melchiork@users.noreply.github.com>
2023-09-24 20:29:18 +0200

    Error scenarios testing

---
+++ ImgwApi.Test/HydroClientGetWithErrorShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace ImgwApi.Test;
+
+public class HydroClientGetWithErrorShould
+{
+    [Theory]
+    [InlineData(HttpStatusCode.BadRequest)]
+    [InlineData(HttpStatusCode.GatewayTimeout)]
+    [InlineData(HttpStatusCode.RequestTimeout)]
+    [InlineData(HttpStatusCode.InternalServerError)]
+    public async Task ThrowExceptionWhenNon2XXCodeIsReturnedForGetAll(HttpStatusCode code)
+    {
+        var client = HydroClient.Create(new HttpClient(new MockHttpMessageHandler(code)));
+
+        var act = async () => await client.GetAllAsync();
+
+        await act.Should().ThrowAsync<ApiClientException>().WithMessage($"Api returned error code {code}");
+    }
+}
