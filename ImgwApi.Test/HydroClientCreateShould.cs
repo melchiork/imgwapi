6f96b0352aec8de63fea54b9e1aeb1c678e3ee03
melchiork <2656932+melchiork@users.noreply.github.com>
2023-09-24 20:23:56 +0200

    Testing hydro client creation

---
+++ ImgwApi.Test/HydroClientCreateShould.cs
@@ -0,0 +1,28 @@
+﻿namespace ImgwApi.Test;
+
+public class HydroClientCreateShould
+{
+    [Fact]
+    public void ReturnNewInstanceWhenNoParamsCtorIsUsed()
+    {
+        var client = HydroClient.Create();
+
+        client.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ReturnNewInstanceWhenProvidedWithHttpClient()
+    {
+        var client = HydroClient.Create(new HttpClient());
+
+        client.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ThrowExceptionWhenNullIsProvided()
+    {
+        Action act = () => HydroClient.Create(null);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+}
